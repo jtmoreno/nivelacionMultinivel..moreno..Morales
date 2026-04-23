efbd5e5debb481054db193e6b3cfd7bd4a06f721
angelamoraleso <angelamoralesoc@gmail.com>
2026-04-23 13:19:18 -0500

    fet: add main

---
+++ java/main.java
@@ -0,0 +1,8 @@
+public static void main (String [] args)
+{
+    PersonajeBase p=new PersonajeBase();
+    System.out.println (p.nombre);
+    System.out.println (p.nivel);
+    System.out.println (p.vida);
+
+}
