696c97debb7fa1810df5dc3717e907faae4559e8
jtmoreno <juancdagarra@gmail.com>
2026-04-23 13:41:08 -0500

    fet: new atributte PersonajeBase, declaration of aragon

--- java/main.java
+++ java/main.java
@@ -7,5 +7,15 @@ public static void main (String [] args)
     System.out.println (p.vida);
     System.out.println (p.puntosMana);
     
+    
+    PersonajeBase p = new PersonajeBase("aragon", 1, 100, 100,
+     true, "guerrero", 15, 10,0);
+    System.out.println(p.nombre);
+    System.out.println(p.nivel);
+    System.out.println(p.vida);
+
+
+    p.toString();
+
 
 }
