d6425f3acfea5e911ebef3286c6da1da30869d2a
angelamoraleso <angelamoralesoc@gmail.com>
2026-04-23 13:39:13 -0500

    Personaje Gandalf

--- java/main.java
+++ java/main.java
@@ -1,10 +1,11 @@
-public static void main(String[] args) {
-    PersonajeBase p = new PersonajeBase("aragon", 1, 100, 100,
-     true, "guerrero", 15, 10,0);
-    System.out.println(p.nombre);
-    System.out.println(p.nivel);
-    System.out.println(p.vida);
+public static void main (String [] args)
+{
+    PersonajeBase p=new PersonajeBase("Gandalf", "Mago",5,80.0, 120);
+    System.out.println (p.nombre);
+    System.out.println (p.clase);
+    System.out.println (p.nivel);
+    System.out.println (p.vida);
+    System.out.println (p.puntosMana);
+    
 
-
-    p.toString();
 }
